72f56bde70b7dfbe8dfea1657aa7ae77b98def56
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2022-02-25 19:48:04 +0700

    update models and admin

--- RestApisV1/ecourse/courses/models.py
+++ RestApisV1/ecourse/courses/models.py
@@ -17,3 +17,19 @@ class ModelBase(models.Model):
 
 class Category(ModelBase):
     name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(ModelBase):
+    subject = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        unique_together = ('subject', 'category')
