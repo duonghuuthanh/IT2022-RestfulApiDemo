45f2e6c82bab4d80d13e00af362a9088ad9da910
Thanh Duong <dhthanhqa@gmail.com>
2022-04-28 12:38:25 +0700

    Updates

---
+++ precache-manifest.0d809a5b226934f624e581f89150c726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391ae7bb84570fc8151fd5c85ee28213",
+    "url": "/ecourse/index.html"
+  },
+  {
+    "revision": "df393d2877a85c7b4409",
+    "url": "/ecourse/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "9508a9d40fc0c2eb1cd0",
+    "url": "/ecourse/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "df393d2877a85c7b4409",
+    "url": "/ecourse/static/js/2.0c68f3b8.chunk.js"
+  },
+  {
+    "revision": "9508a9d40fc0c2eb1cd0",
+    "url": "/ecourse/static/js/main.4cb5a501.chunk.js"
+  },
+  {
+    "revision": "0f46b6483170a14a806b",
+    "url": "/ecourse/static/js/runtime~main.4295fb29.js"
+  }
+]);
